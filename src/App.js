8c5afadca57c50b6e535e8603411e2459e7dca90
Naweli Verma <Naweli.Verma@propertyloop.co.uk>
2022-10-07 23:29:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ width: "100%", position: "relative" }}>
+      <img
+        src="https://images.unsplash.com/photo-1665149368357-864968813478?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        alt="background"
+        style={{ width: "100%" }}
+      />
+      <h1
+        style={{
+          position: "absolute",
+          top: "100px",
+          right: "230px",
+          fontSize: "5rem",
+        }}
+      >
+        Netlify Session without CLI
+      </h1>
     </div>
   );
 }
